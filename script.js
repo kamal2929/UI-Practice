692f664a12035b6947986c560e2e9aec06420151
KAMAL A <95035265+kamal2929@users.noreply.github.com>
2024-05-17 13:56:13 +0530

    added overlay effect need to fix toggle when clicking new container

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 document.getElementById('new').onclick=function(event){
-
     const newContainer=document.getElementById('newContainer');
     if(newContainer.style.right==='-670px'){        
         newContainer.style.right='0';
+        overlay.style.display = 'block'; 
     }
     else{
         newContainer.style.right='-670px'
+        overlay.style.display = 'none'; 
     }
     event.stopPropagation();
+}
+document.getElementById('body').onclick=function()
+{
+    const newContainer=document.getElementById('newContainer');
+    if(newContainer.style.right==='0px'){        
+        newContainer.style.right='-670px';
+        overlay.style.display = 'none';
+    }
 }
